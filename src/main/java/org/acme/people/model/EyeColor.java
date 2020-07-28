8673a7cd62a30dc7a60a28e339272be6c40c6b42
wserafin <wserafin@gmail.com>
2020-07-28 15:25:20 -0400

    fixed

---
+++ src/main/java/org/acme/people/model/EyeColor.java
@@ -0,0 +1,4 @@
+package org.acme.people.model;
+public enum EyeColor {
+    BLUE, GREEN, HAZEL, BROWN
+}
--- src/main/java/org/acme/people/rest/GreetingResource.java
+++ src/main/java/org/acme/people/rest/GreetingResource.java
@@ -1,10 +1,13 @@
 package org.acme.people.rest;
 
+import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import org.acme.people.service.GreetingService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -18,4 +21,15 @@ public class GreetingResource {
     public String hello() {
         return "hello";
     }
+
+    @Inject
+    GreetingService service;
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    @Path("/greeting/{name}")
+    public String greeting(@PathParam("name") String name) {
+        return service.greeting(name);
+    }
+
 }
